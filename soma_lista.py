e611c9bf8b2a2c7e09e8e35d8dd64c02c8779ad7
Estherbns <estherbns@hotmail.com>
2026-06-03 18:15:24 -0300

    commit 8

--- soma_lista.py
+++ soma_lista.py
@@ -4,5 +4,5 @@ for i in range(len(lista)):
    i = i + 1  
    soma = soma + lista[i-1]
    
-print("A soma dos elementos da lista é: ", soma)
+print("A soma dos elementos da lista é: ", soma) #---
   
